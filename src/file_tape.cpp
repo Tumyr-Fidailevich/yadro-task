1b4f486937dc7d03f4e3201faa68add0a960114d
Timur <timurkashapov15@gmail.com>
2024-03-20 21:24:22 +0300

    add def of file_tape constructor

--- src/file_tape.cpp
+++ src/file_tape.cpp
@@ -3,38 +3,41 @@
 
 const std::size_t file_tape::FILL_DIGITS_NUMBER = std::to_string(std::numeric_limits<int>::min()).size();
 const char file_tape::FILL_CHAR = ' ';
+static const std::filesystem DEFAULT_TAPE_CONFIG_PATH = ".." / "default_tape.cfg";
 
 
-file_tape::config::config(const std::string& filename) 
+file_tape::config::config(const std::filesystem::path& filepath) 
 {
-    std::fstream configFile(filename);
-    readTime = std::chrono::milliseconds(0);
-    writeTime = std::chrono::milliseconds(0);
-    moveTime = std::chrono::milliseconds(0);
-    rewindTime = std::chrono::milliseconds(0);
-}
-
-file_tape::file_tape(const std::string& filename, const config& cfg) : _file(filename, std::ios::in, std::ios::out), _config(cfg)
-{
-    
-}
-
-int file_tape::read() const
-{
-    return {};
-}
-
-void file_tape::write(int value)
-{
+    std::ifstream configFile(filepath);
 
 }
 
-bool file_tape::move_left() const
+file_tape::file_tape(const std::filesystem::path& filepath, const config& cfg) : _file(filepath, std::ios::in, std::ios::out), _config(cfg), _pos(0)
+{
+    if(!_file.is_open())
+    {
+        throw std::runtime_error("Cannot open or create " + filepath.filename());
+    }
+
+    _size = (std::filesystem::file_size(filepath) + 1) / (FILL_DIGITS_NUMBER + 1);
+}
+
+int file_tape::read() const noexcept
 {
     return {};
 }
 
-bool file_tape::move_right() const
+void file_tape::write(int value) noexcept
+{
+
+}
+
+bool file_tape::move_left() const noexcept
+{
+    return {};
+}
+
+bool file_tape::move_right() const noexcept
 {
     return {};
 }
