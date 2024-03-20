0e0c07bde4e8d2b6e60b1f7e6f14bcb9345fcefb
Timur <timurkashapov15@gmail.com>
2024-03-20 18:51:31 +0300

    add sorting algorithm decl

---
+++ include/tape_sorting.h
@@ -0,0 +1,11 @@
+#ifndef TAPE_SORTING_H
+#define TAPE_SORTING_H
+
+#include "pch.h"
+#include "tape_interface.h"
+
+
+void tape_sorting(const tape_interface& source_tape, tape_interface& dst_tape);
+
+
+#endif //TAPE_SORTING_H
---
+++ src/tape_sorting.cpp
@@ -0,0 +1,7 @@
+#include "tape_sorting.h"
+
+
+void tape_sorting(const tape_interface& source_tape, tape_interface& dst_tape)
+{
+
+}
